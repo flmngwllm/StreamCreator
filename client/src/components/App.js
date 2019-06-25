f2378f067883752527a282120a50fe409928ab49
William Fleming <chaosmegaman@Williams-MacBook-Pro.local>
2019-06-25 19:53:05 -0400

    added routes for various stream components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,10 @@
 import React,{Component} from 'react'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
 
 
@@ -9,7 +14,11 @@ class App extends Component{
         return(
             <div>
                 <Router>
-                    
+                <Route path="/" exact component ={StreamList}/>
+                <Route path="/streams/create" exact component ={StreamCreate}/>
+                <Route path="/streams/edit" exact component ={StreamEdit}/>
+                <Route path="/streams/delete" exact component ={StreamDelete}/>
+                <Route path="/streams/show" exact component ={StreamShow}/>
                 </Router>
             </div>
         )
