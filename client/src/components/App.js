1400d6f958cec0f19b41623f712fa3ff2c2992ad
William Fleming <chaosmegaman@Williams-MacBook-Pro.local>
2019-06-25 19:44:04 -0400

    created stream folder in components then added Streamedit, delete, show, list, and create components with boilerplate code

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,16 @@
 import React,{Component} from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+
+
+
 
 class App extends Component{
     render() {
         return(
             <div>
-                App
+                <Router>
+                    
+                </Router>
             </div>
         )
     }
