8ebcf18700010819bca3fbf997b740b3efaed4b2
William Fleming <chaosmegaman@Williams-MacBook-Pro.local>
2019-06-26 16:12:09 -0400

    added google client to auth component

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,6 +1,17 @@
 import React,{Component} from 'react'
 
 class GoogleAuth extends Component{
+    //loads google client api
+    componentDidMount(){
+        window.gapi.load('client:auth2', () =>{
+            window.gapi.client.init({
+                clientId: 
+                '558643926580-o0asm8aqs5q2sna7ckktri6fu8jeej17.apps.googleusercontent.com',
+                scope: 'email'
+            })
+        })
+    }
+
     render(){
         return(
 <div>
