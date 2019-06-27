e70184303fffc5e6960ab98425c88f3e56d3cc77
William Fleming <chaosmegaman@Williams-MacBook-Pro.local>
2019-06-27 12:44:41 -0400

    initialized the client and added a callback that checks to see if user is signed in

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,13 +1,24 @@
 import React,{Component} from 'react'
 
 class GoogleAuth extends Component{
+
+state = {
+    isSignedIn: null
+}
+
     //loads google client api
     componentDidMount(){
-        window.gapi.load('client:auth2', () =>{
+        //pass a callback function after client librabry is invoked
+        window.gapi.load('client:auth2', () => {
+            // calls asynchronus network request to initialize the client
             window.gapi.client.init({
                 clientId: 
                 '558643926580-o0asm8aqs5q2sna7ckktri6fu8jeej17.apps.googleusercontent.com',
                 scope: 'email'
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance()
+                // sets the state and checks to see if user is signed in
+                this.setState({isSignedIn: this.auth.isSignedIn.get() })
             })
         })
     }
