11d2db77de2503245e29f039b64a4bd2708fe5c2
William Fleming <chaosmegaman@Williams-MacBook-Pro.local>
2019-06-30 14:30:45 -0400

    created a types file and created a variable that holds our types for signin and signout

---
+++ client/src/actions/types.js
@@ -0,0 +1,4 @@
+
+//assigned the types to const variable
+export const SIGN_IN = 'SIGN_IN'
+export const SIGN_OUT = 'SIGN_OUT'
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,3 +1,4 @@
+import {SIGN_IN,SIGN_OUT} from '../actions/types'
 // defaulted the value
 const INITIAL_STATE = {
     isSignedIn: null
@@ -6,9 +7,9 @@ const INITIAL_STATE = {
 //passed the state object and action then created a switch case 
 export default (state = INITIAL_STATE, action) => {
     switch (action.type){
-        case 'SIGN_IN':
+        case SIGN_IN:
             return {...state, isSignedIn: true}
-            case 'SIGN_OUT':
+            case SIGN_OUT:
             return {...state, isSignedIn: false}
             default:
                 return state
