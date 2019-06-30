b20e988d54329e5c1cd897cb05997617ae2d9b34
William Fleming <chaosmegaman@Williams-MacBook-Pro.local>
2019-06-30 18:35:26 -0400

    created a renderError function that checks if fields have been touched and if they have an error then called the function in the renderInput function with meta prop

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -2,13 +2,28 @@ import React,{Component} from 'react'
 import {Field, reduxForm} from 'redux-form'
 
 class StreamCreate extends Component{
+
+//function that checks if the user touches the form and if there is an error message
+renderError({error, touched}) {
+    if(touched && error){
+        return (
+            <div className="ui error message">
+                <div className ="header">{error}</div>
+            </div>
+        )
+    }
+}
+
+
 //helper function that renders the fields
-renderInput = ({input, label}) =>{
+renderInput = ({input, label, meta}) =>{
+    const className = `field ${meta.error & meta.touched ? 'error' : ''}`
     return (
     //takes all the formProps properties and adds them as properties to the input element
-    <div className="field">
+    <div className={className}>
         <label>{label}</label>
-    <input {...input}/>
+    <input {...input} autoComplete="off"/>
+        {this.renderError(meta)}
     </div>
     )
 }
@@ -20,8 +35,8 @@ onSubmit(formValues){
         return (
             
        
-            <form 
-            onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form"> 
+            <form className="ui form error"
+            onSubmit={this.props.handleSubmit(this.onSubmit)} > 
                 {/* Fields from redux from library that takes in helper function to pass to the components */}
                 <Field name="title" component={this.renderInput} label="Enter Title"/>
                 <Field name="description" component={this.renderInput} label="Enter Description"/>
@@ -40,13 +55,13 @@ const validate = (formValues) => {
  }
 
  if(!formValues.description){
-    errors.description = "You must enter a title"
+    errors.description = "You must enter a description"
  }
 
  return errors
 }
 
 export default reduxForm({
-    //name of the form
-    form: 'streamCreate'
+    //name of the form and connected validate function so it can be called
+    form: 'streamCreate', validate: validate
 })(StreamCreate);
