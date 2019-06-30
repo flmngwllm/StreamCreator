e318bf6bad2741ef1e66dbc4b85882778d6f9b9f
William Fleming <chaosmegaman@Williams-MacBook-Pro.local>
2019-06-30 13:56:20 -0400

    added connect with actions to auth component

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,4 +1,6 @@
 import React,{Component} from 'react'
+import {connect} from 'react-redux'
+import {signIn, signOut} from '../actions'
 
 class GoogleAuth extends Component{
 
@@ -24,8 +26,12 @@ state = {
         })
     }
 //function is called anytime the users authentication statues is changed
-onAuthChange = () => {
-this.setState({isSignedIn: this.auth.isSignedIn.get() })
+onAuthChange = (isSignedIn) => {
+if (isSignedIn){
+    this.props.signIn()
+} else {
+    this.props.signOut()
+}
 }
 
 
@@ -72,4 +78,4 @@ renderAuthButton(){
     }
 }
 
-export default GoogleAuth;
+export default connect(null, {signIn, signOut})(GoogleAuth);
