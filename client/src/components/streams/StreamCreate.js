18207d55fabc7f8ee3e3849bde1f90edb622b428
William Fleming <chaosmegaman@Williams-MacBook-Pro.local>
2019-06-30 18:08:16 -0400

    created a validate function to check if user entered valid input

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -13,18 +13,38 @@ renderInput = ({input, label}) =>{
     )
 }
 
+onSubmit(formValues){
+    console.log(formValues)
+}
     render(){
         return (
-            <form className="ui form"> 
+            
+       
+            <form 
+            onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form"> 
                 {/* Fields from redux from library that takes in helper function to pass to the components */}
                 <Field name="title" component={this.renderInput} label="Enter Title"/>
                 <Field name="description" component={this.renderInput} label="Enter Description"/>
+
+                <button className="ui button primary">Submit</button>
             </form>
         )
     }
 }
 
+//checks to see if user enters valid input
+const validate = (formValues) => {
+    const errors = {}
+ if(!formValues.title){
+    errors.title = "You must enter a title"
+ }
 
+ if(!formValues.description){
+    errors.description = "You must enter a title"
+ }
+
+ return errors
+}
 
 export default reduxForm({
     //name of the form
