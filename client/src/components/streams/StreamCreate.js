ab81810343d6ed6effd4988a86c3768d59db954c
William Fleming <chaosmegaman@Williams-MacBook-Pro.local>
2019-06-30 17:48:16 -0400

    created a form reducer using redux-form library and connected redux forms in the createStrem component

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React,{Component} from 'react'
+import {Field, reduxForm} from 'redux-form'
 
-const StreamCreate = () => {
-    return <div>StreamCreate</div>;
+class StreamCreate extends Component{
+//helper function that renders the fields
+renderInput = (formProps) =>{
+    return (
+    //takes all the formProps properties and adds them as properties to the input element
+    <input {...formProps.input}/>
+    )
+}
+
+    render(){
+        return (
+            <form> 
+                <Field name="title" component={this.renderInput} />
+                <Field name="description" component={this.renderInput}/>
+            </form>
+        )
+    }
 }
 
 
-export default StreamCreate;
+
+export default reduxForm({
+    //name of the form
+    form: 'streamCreate'
+})(StreamCreate);
