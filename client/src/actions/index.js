2e8aebc0e0821e46fbd99defe1b14d01f20ce973
William Fleming <chaosmegaman@Williams-MacBook-Pro.local>
2019-07-01 16:25:57 -0400

    created action creators for all our REST based actions for our streams

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -24,17 +24,25 @@ return {
      }
  }
 
-export const fetchStreams = () => {
+export const fetchStreams = () => async dispatch => {
+
+    const response = await streams.get('/streams')
+    dispatch({type: FETCH_STREAMS, payload: response.data})
 
 }
 
-export const fetchStream = () => {
-
+export const fetchStream = (id) => async dispatch => {
+    const response = await streams.get(`/streams/${id}`)
+    dispatch({type: FETCH_STREAM, payload: response.data})
 }
 
-export const deleteStream = () => {
-
+export const deleteStream = (id) => async dispatch => {
+    await streams.delete(`/streams/${id}`)
+    dispatch({type: DELETE_STREAM, payload: id})
 }
- export const editStream = () =>{
 
+
+ export const editStream = (formValues, id) => async dispatch => {
+    const response = await streams.put(`/streams/${id}`, formValues)
+    dispatch({type: EDIT_STREAM, payload: response.data})
  }
