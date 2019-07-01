37fa4def34a54bf94266694426cdc9bd039d7a10
William Fleming <chaosmegaman@Williams-MacBook-Pro.local>
2019-07-01 19:09:40 -0400

    added pulled isSignedIn from auth state to mapStateToProps then created a renderCreated method that checks if the useer is signed in that creates a create stream button if they are

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,6 +1,7 @@
 import React,{Component} from 'react'
 import {connect} from 'react-redux'
 import { fetchStreams } from '../../actions'
+import {Link} from 'react-router-dom'
 
 class StreamList extends Component{
 // get a list of the streams
@@ -39,11 +40,27 @@ class StreamList extends Component{
             )
         })
     }
+
+
+    //renders a create stream button if ther user is signed in
+    renderCreate(){
+        if(this.props.isSignedIn){
+            return (
+                <div style={{textAlign : 'right'}}>
+                    <Link to="streams/new" className="ui button primary">
+                    Create Stream
+                    </Link>
+                </div>
+            )
+        }
+    }
+
     render() {
         return (
             <div>
             <h2>Streams</h2>
             <div className="ui celled list">{this.renderList()}</div>
+            {this.renderCreate()}
             </div>
         )
     }
@@ -54,7 +71,9 @@ const mapStateToProps = (state) => {
     //takes object as an argument then takes the values then insert them into an array
     return {streams: Object.values(state.streams),
     //getting the id of the person currently signed in from redux store
-    currentUserId: state.auth.userId }
+    currentUserId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn }
+    
 }
 
 
