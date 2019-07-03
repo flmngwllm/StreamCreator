d1932a5ed9d473c76d4989fc7ceaf7b0e8bd4b0c
William Fleming <chaosmegaman@Williams-MacBook-Pro.local>
2019-07-03 16:31:34 -0400

    created a new component streamForm and added all the code from streamCreate and refactored it so the form can be used between components

---
+++ client/src/components/streams/StreamForm.js
@@ -0,0 +1,68 @@
+import React,{Component} from 'react'
+import {Field, reduxForm} from 'redux-form'
+
+class StreamForm extends Component{
+
+//function that checks if the user touches the form and if there is an error message
+renderError({error, touched}) {
+    if(touched && error){
+        return (
+            <div className="ui error message">
+                <div className ="header">{error}</div>
+            </div>
+        )
+    }
+}
+
+
+//helper function that renders the fields
+renderInput = ({input, label, meta}) =>{
+    const className = `field ${meta.error & meta.touched ? 'error' : ''}`
+    return (
+    //takes all the formProps properties and adds them as properties to the input element
+    <div className={className}>
+        <label>{label}</label>
+    <input {...input} autoComplete="off"/>
+        {this.renderError(meta)}
+    </div>
+    )
+}
+
+onSubmit = (formValues)=> {
+    this.props.onSubmit(formValues)
+}
+    render(){
+        return (
+            
+       
+            <form className="ui form error"
+            onSubmit={this.props.handleSubmit(this.onSubmit)} > 
+                {/* Fields from redux from library that takes in helper function to pass to the components */}
+                <Field name="title" component={this.renderInput} label="Enter Title"/>
+                <Field name="description" component={this.renderInput} label="Enter Description"/>
+
+                <button className="ui button primary">Submit</button>
+            </form>
+        )
+    }
+}
+
+//checks to see if user enters valid input
+const validate = (formValues) => {
+    const errors = {}
+ if(!formValues.title){
+    errors.title = "You must enter a title"
+ }
+
+ if(!formValues.description){
+    errors.description = "You must enter a description"
+ }
+
+ return errors
+}
+
+export default reduxForm({
+    //name of the form and connected validate function so it can be called
+    form: 'streamForm', validate: validate
+})(StreamForm);
+
