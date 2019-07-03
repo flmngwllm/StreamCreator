d644d60b967415d051a81ff555158560656f1f4d
William Fleming <chaosmegaman@Williams-MacBook-Pro.local>
2019-07-03 16:19:31 -0400

    made the editStream a class based component and created a mapStateToProps function with ownProps so it can access the properties from the stream and match its id

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React,{Component} from 'react'
+import { connect } from 'react-redux'
+import { fetchStream, editStream } from '../../actions';
 
-const StreamEdit = (props) => {
-    console.log(props)
-    return <div>StreamEdit</div>;
+class StreamEdit extends Component{
+    componentDidMount(){
+        //fetch our single stream then pass in the id of the stream we want
+        this.props.fetchStream(this.props.match.params.id);
+    }
+    
+    render(){
+    console.log(this.props)
+    return (<div>StreamEdit</div>
+     )
+    }
+}
+    
+//selects the stream out of object that contains our streams then assigns it to the stream property then return from mapStateToProps
+const mapStateToProps = (state, ownProps) => {
+    return {stream: state.streams[ownProps.match.params.id]}
 }
 
-
-export default StreamEdit;
+export default  connect(mapStateToProps, {fetchStream: fetchStream})(StreamEdit);
