1258f458e661b56c26c84463033227947ccd2cb6
William Fleming <chaosmegaman@Williams-MacBook-Pro.local>
2019-07-07 15:35:48 -0400

    updated the delete route on the Appcomponent

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Modal from '../Modal'
 import history from '../../history'
+import actions from '../../actions'
 
 //Delete Stream component
 const StreamDelete = () => {
