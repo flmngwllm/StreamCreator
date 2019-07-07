fdcfa77b2305ad5d00067a9a65085a7782ea7caf
William Fleming <chaosmegaman@Williams-MacBook-Pro.local>
2019-07-07 15:50:18 -0400

    connected fetchStream action to streamDelete component and add a componentDidMount that gets the a single stream with its id

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,12 +1,16 @@
 import React, {Component} from 'react'
 import Modal from '../Modal'
 import history from '../../history'
-import actions from '../../actions'
+import {fetchStream} from '../../actions'
+import connect from 'react-redux'
+
 
 //Delete Stream component
 class StreamDelete extends Component {
 
-
+componentDidMount () {
+    this.props.fetchStream(this.props.match.params.id)
+}
 
     renderActions (){
         return (
@@ -14,7 +18,7 @@ class StreamDelete extends Component {
             <button className="ui button negative">Delete</button>
             <button className="ui button cancel">Cancel</button>
         </React.Fragment>
-    )
+        )
     }
 
 
@@ -36,4 +40,4 @@ class StreamDelete extends Component {
 
 
 
-export default StreamDelete;
+export default connect(null, {fetchStream : fetchStream}) (StreamDelete);
