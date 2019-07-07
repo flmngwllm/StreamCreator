ccbd0396c303899ab17fa63261b943ea93163435
William Fleming <chaosmegaman@Williams-MacBook-Pro.local>
2019-07-07 14:48:37 -0400

    removed the modal divfrom the root in the index.html also imported modal component into streamDelete component

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Modal = (props) => {
+const Modal = props => {
     return ReactDOM.createPortal(
-        <div className=" ui dimmer modals visible active">
+        <div className="ui dimmer modals visible active">
             <div className="ui standard modal visible active">
                 kajfkljsdklfjkl djfljklfklj
             </div>
-        </div>, document.querySelector('#modal')
+        </div>, 
+        document.querySelector('#modal')
     )
 }
 
