b1bb4446a366dea8a411d8d257320fa13a7da1a1
William Fleming <chaosmegaman@Williams-MacBook-Pro.local>
2019-07-07 14:53:50 -0400

    added a title, some content and a delete and cancel button to the modal

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -5,7 +5,12 @@ const Modal = props => {
     return ReactDOM.createPortal(
         <div className="ui dimmer modals visible active">
             <div className="ui standard modal visible active">
-                kajfkljsdklfjkl djfljklfklj
+                <div className="header">Delete Stream</div>
+                <div className="content">Are you sure you want to delete this Stream?</div>
+                <div className="actions">
+                    <button className="ui primary button">Delete</button>
+                    <button className="ui button">Delete</button>
+                    </div>
             </div>
         </div>, 
         document.querySelector('#modal')
